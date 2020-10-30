46f78a8d47c9989d62b56bfe2d73d522cc8930c6
63070086 <69414436+63070086@users.noreply.github.com>
2020-10-30 15:14:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
-<html>
-<body>
-<?php
-echo "<h2>PHP is Fun</h2>";
-echo "<h2>Hellow TestWeb</h2>";
-?>
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WEB TEST</title>
+  </head>
+  <body>
+      <h1>ธีรภพ โพธิ์เกิด ชื่อเล่น อ๊อฟ</h1>
+      <img src="images.jpg">
+      <p>Paragraph</p>
+      <img src="picture1.jpg">
+      <h1><a href="https://www.it.kmitl.ac.th/th/">This is a Link</a></h>
+      <h2>ลูก: วันนี้ที่โรงเรียนสุดยอดมากเลยแม่ พวกเราทำระเบิดกันด้วย
+แม่: ว้าวๆ ทำอะไรสุดโต่งไปเลยนะ โรงเรียนของลูกเนี่ย แล้วพรุ่งนี้ลูกจะทำอะไรที่โรงเรียนเหรอ
+ลูก: โรงเรียน? ยังมีโรงเรียนอยู่เหรอแม่?</h2>
+      <ul>
+        <li>Google</li>
+        <li>Yahoo</li>
+        <li>Amazon</li>
+      </ul>
+  </body>
 </html>
